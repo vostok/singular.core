e7cf5ce9dae2f29c5449f925f1423da68cef2563
Татьяна Гладышева <gladysheva@skbkontur.ru>
2020-04-28 22:44:56 +0500

    Add Serializable attribut for settings

--- Vostok.Singular.Core/Settings/NonIdempotencySignSettings.cs
+++ Vostok.Singular.Core/Settings/NonIdempotencySignSettings.cs
@@ -1,5 +1,8 @@
-﻿namespace Vostok.Singular.Core.Settings
+﻿using System;
+
+namespace Vostok.Singular.Core.Settings
 {
+    [Serializable]
     internal class NonIdempotencySignSettings
     {
         public string Method;
--- Vostok.Singular.Core/Settings/NonIdempotencySignsSettings.cs
+++ Vostok.Singular.Core/Settings/NonIdempotencySignsSettings.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Vostok.Singular.Core.Settings
 {
+    [Serializable]
     internal class NonIdempotencySignsSettings
     {
         public List<NonIdempotencySignSettings> Signs = new List<NonIdempotencySignSettings>();
