7920af1dac98713c33e8b6dbcb6fdbdce0a2a425
Илья Бобрышев <bis@skbkontur.ru>
2020-05-24 07:31:53 +0500

    Cache reborn

---
+++ Vostok.Singular.Core/Idempotency/IdempotencyIdentifierCache.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Concurrent;
+using Vostok.Configuration;
+using Vostok.Configuration.Logging;
+using Vostok.Logging.Abstractions;
+using Vostok.Singular.Core.Idempotency.BlackList;
+using Vostok.Singular.Core.Idempotency.IdempotencyControlRules;
+
+namespace Vostok.Singular.Core.Idempotency
+{
+    internal static class IdempotencyIdentifierCache
+    {
+        private static readonly ConcurrentDictionary<string, Lazy<IIdempotencyIdentifier>> Cache = new ConcurrentDictionary<string, Lazy<IIdempotencyIdentifier>>();
+
+        static IdempotencyIdentifierCache()
+        {
+            var providerSettings = new ConfigurationProviderSettings()
+                .WithErrorLogging(LogProvider.Get())
+                .WithSettingsLogging(LogProvider.Get());
+
+            var provider = new ConfigurationProvider(providerSettings);
+
+            if (!ConfigurationProvider.TrySetDefault(provider))
+                provider.Dispose();
+        }
+
+        public static IIdempotencyIdentifier Get(string serviceName)
+        {
+            return Cache.GetOrAdd(serviceName, s => new Lazy<IIdempotencyIdentifier>(() => Create(s))).Value;
+        }
+
+        private static IIdempotencyIdentifier Create(string serviceName)
+        {
+            var settingsProvider = new SettingsProvider(serviceName);
+            var idempotencySignsCache = new NonIdempotencySignsCache(new NonIdempotencySignsSettingsProvider(settingsProvider));
+            return new IdempotencyIdentifier(
+                new BlackListIdempotencyResolver(idempotencySignsCache),
+                new IclResolver(new IclCache(new IclRulesSettingsProvider(settingsProvider)))
+            );
+        }
+    }
+}
