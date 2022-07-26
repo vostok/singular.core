b2543bc8a382cc9666c72de1d6ee1e1bf7c86e93
lunev.d <lunev.d@kontur.ru>
2022-07-26 15:49:05 +0500

    Remove request extensions

--- Vostok.Singular.Core/PathPatterns/Extensions/RequestExtensions.cs
+++ Vostok.Singular.Core/PathPatterns/Extensions/RequestExtensions.cs
@@ -4,12 +4,6 @@ namespace Vostok.Singular.Core.PathPatterns.Extensions
 {
     internal static class RequestExtensions
     {
-        internal static Request WithNotIdempotentHeader(this Request request) =>
-            request.WithHeader(SingularHeaders.Idempotent, false);
-
-        internal static Request WithIdempotentHeader(this Request request) =>
-            request.WithHeader(SingularHeaders.Idempotent, true);
-
         internal static string GetIdempotencyHeader(this Request request) =>
             request.Headers?[SingularHeaders.Idempotent];
     }
