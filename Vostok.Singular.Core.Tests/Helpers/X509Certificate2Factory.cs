1661ef29b0f9685734655614994064f3263c0e07
Ilya Tzoop <ilyatzoop@gmail.com>
2022-08-22 16:41:22 +0500

    Added tests for TLS

---
+++ Vostok.Singular.Core.Tests/Helpers/X509Certificate2Factory.cs
@@ -0,0 +1,62 @@
+﻿#if NETCOREAPP2_0_OR_GREATER
+using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Vostok.Singular.Core.Tests.Helpers
+{
+    internal static class X509Certificate2Factory
+    {
+        public static X509Certificate2 Create(string commonName)
+        {
+            return Create(commonName, DateTimeOffset.Now, DateTimeOffset.Now.AddYears(5));
+        }
+
+        public static X509Certificate2 Create(string commonName, DateTimeOffset notBefore, DateTimeOffset notAfter)
+        {
+            var req = GetCertificateRequest(commonName);
+            // NOTE (tsup, 22-08-2022): Dirty hacks for signing certificates on the fly
+            req.CertificateExtensions.Add(new X509BasicConstraintsExtension(new AsnEncodedData(new byte[] {0x30, 0x06, 0x01, 0x01, 0xFF, 0x02, 0x01, 0x01}), false));
+            return req.CreateSelfSigned(notBefore, notAfter);
+        }
+
+        public static X509Certificate2 CreateSigned(string commonName, DateTimeOffset notBefore, DateTimeOffset notAfter, X509Certificate2 signer)
+        {
+            var req = GetCertificateRequest(commonName);
+            return req.Create(signer, notBefore, notAfter, new byte[] {1, 2, 3, 4, 5});
+        }
+
+        public static X509Certificate2 CreateSigned(string commonName, X509Certificate2 signer)
+        {
+            return CreateSigned(commonName, DateTimeOffset.Now, DateTimeOffset.Now.AddYears(5), signer);
+        }
+
+        public static X509Certificate2 CreateDefault()
+        {
+            return Create("Singular");
+        }
+
+        public static X509Certificate2 CreateDefaultExpired()
+        {
+            return Create("Singular", DateTimeOffset.Now - TimeSpan.FromDays(365), DateTimeOffset.Now - TimeSpan.FromDays(364));
+        }
+
+        public static X509Chain CreateChainFromCertificates(X509Certificate2 certificate, params X509Certificate2[] extra)
+        {
+            var chain = new X509Chain();
+
+            foreach (var parentCertificate in extra)
+                chain.ChainPolicy.ExtraStore.Add(parentCertificate);
+
+            chain.Build(certificate);
+            return chain;
+        }
+
+        private static CertificateRequest GetCertificateRequest(string commonName)
+        {
+            var ecdsa = ECDsa.Create();
+            return new CertificateRequest($"cn={commonName}", ecdsa!, HashAlgorithmName.SHA256);
+        }
+    }
+}
+#endif
---
+++ Vostok.Singular.Core.Tests/Tls/SingularHandshakeValidator_Tests.cs
@@ -0,0 +1,78 @@
+﻿using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Vostok.Logging.Abstractions;
+using Vostok.Singular.Core.Tls;
+#if NETCOREAPP2_0_OR_GREATER
+using Vostok.Singular.Core.Tests.Helpers;
+#endif
+
+namespace Vostok.Singular.Core.Tests.Tls
+{
+    [TestFixture]
+    internal class SingularHandshakeValidator_Tests
+    {
+        private SingularHandshakeValidator handshakeValidator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var verifier = Substitute.For<ICertificateChainVerifier>();
+            verifier.VerifyChain(null).ReturnsForAnyArgs(true);
+            handshakeValidator = new SingularHandshakeValidator(verifier, new SilentLog());
+        }
+
+        [Test]
+        public void Should_return_true_if_OS_trusts()
+        {
+            handshakeValidator.Verify(null, null, null, SslPolicyErrors.None).Should().BeTrue();
+        }
+
+        [TestCase(SslPolicyErrors.RemoteCertificateNameMismatch, Description = "Certificate name does not match FQDN")]
+        [TestCase(SslPolicyErrors.RemoteCertificateNotAvailable, Description = "Certificate is not present or unavailable")]
+        public void Should_not_allow_policy_errors_except_chain_errors(SslPolicyErrors error)
+        {
+            handshakeValidator.Verify(null, null, null, error).Should().BeFalse();
+        }
+
+#if NETCOREAPP2_0_OR_GREATER
+        [Test]
+        public void Should_allow_self_signed_certificates()
+        {
+            var generated = X509Certificate2Factory.CreateDefault();
+            var chain = X509Certificate2Factory.CreateChainFromCertificates(generated);
+            handshakeValidator.Verify(null, generated, chain, SslPolicyErrors.RemoteCertificateChainErrors).Should().BeTrue();
+        }
+
+        [Test]
+        public void Should_not_allow_untrusted_chains()
+        {
+            var generated = X509Certificate2Factory.CreateDefault();
+            var chain = X509Certificate2Factory.CreateChainFromCertificates(generated);
+            var verifier = Substitute.For<ICertificateChainVerifier>();
+            verifier.VerifyChain(null).ReturnsForAnyArgs(false);
+            handshakeValidator = new SingularHandshakeValidator(verifier, new SilentLog());
+            handshakeValidator.Verify(null, generated, chain, SslPolicyErrors.RemoteCertificateChainErrors).Should().BeFalse();
+        }
+
+        [Test]
+        public void Should_not_allow_expired_certificates()
+        {
+            var generated = X509Certificate2Factory.CreateDefaultExpired();
+            var chain = X509Certificate2Factory.CreateChainFromCertificates(generated);
+            handshakeValidator.Verify(null, generated, chain, SslPolicyErrors.RemoteCertificateChainErrors).Should().BeFalse();
+        }
+
+        [Test]
+        public void Should_not_allow_expired_certificates_in_chain()
+        {
+            var root = X509Certificate2Factory.CreateDefaultExpired();
+            var leaf = X509Certificate2Factory.CreateSigned("child", root);
+            var chain = X509Certificate2Factory.CreateChainFromCertificates(leaf, root);
+            handshakeValidator.Verify(null, leaf, chain, SslPolicyErrors.RemoteCertificateChainErrors).Should().BeFalse();
+        }
+#endif
+    }
+}
