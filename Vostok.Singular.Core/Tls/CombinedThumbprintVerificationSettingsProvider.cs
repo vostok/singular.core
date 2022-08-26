504a607c3e869a5e3ff39484329165bdd4cba65a
Ilya Tzoop <ilyatzoop@gmail.com>
2022-08-26 19:01:47 +0500

    Moved ClusterConfig related sources to `vostok.singular.core`.

---
+++ Vostok.Singular.Core/Tls/CombinedThumbprintVerificationSettingsProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Vostok.Commons.Collections;
+
+namespace Vostok.Singular.Core.Tls
+{
+    internal class CombinedThumbprintVerificationSettingsProvider : IThumbprintVerificationSettingsProvider
+    {
+        private readonly IThumbprintVerificationSettingsProvider first;
+        private readonly IThumbprintVerificationSettingsProvider second;
+        private readonly CachingTransform<(IList<string>, IList<string>), IList<string>> blacklistTransform;
+        private readonly CachingTransform<(IList<string>, IList<string>), IList<string>> whitelistTransform;
+
+        public CombinedThumbprintVerificationSettingsProvider(IThumbprintVerificationSettingsProvider first, IThumbprintVerificationSettingsProvider second)
+        {
+            this.first = first;
+            this.second = second;
+            blacklistTransform = new CachingTransform<(IList<string>, IList<string>), IList<string>>(Concat);
+            whitelistTransform = new CachingTransform<(IList<string>, IList<string>), IList<string>>(Concat);
+        }
+
+        public IList<string> GetBlacklist() => blacklistTransform.Get((first.GetBlacklist(), second.GetBlacklist()));
+
+        public IList<string> GetWhitelist() => whitelistTransform.Get((first.GetWhitelist(), second.GetWhitelist()));
+
+        private static IList<string> Concat((IList<string>, IList<string>) x) => x.Item1.Concat(x.Item2).ToList();
+    }
+}
